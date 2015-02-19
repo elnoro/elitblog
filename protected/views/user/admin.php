9229749aa8737cf21fdbdd80a088d1e320412fbd
elnoro <karimov.work@gmail.com>
2015-02-19 06:37:26 +0500

    remove password from admin table

--- protected/views/user/admin.php
+++ protected/views/user/admin.php
@@ -49,7 +49,6 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		'first_name',
 		'second_name',
 		'login',
-		'password',
 		array(
 			'class'=>'CButtonColumn',
 		),
